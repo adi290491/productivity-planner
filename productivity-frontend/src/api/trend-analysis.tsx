26d37442f7c91b1d7edc901e8020314506672eb3
adityasawant.294 <adityasawant.294@gmail.com>
2025-09-14 03:22:38 +0000

    carousel changes

--- productivity-frontend/src/api/trend-analysis.tsx
+++ productivity-frontend/src/api/trend-analysis.tsx
@@ -1,16 +1,18 @@
 import type { DailyTrendResponse, WeeklyTrendResponse } from "../types/trend";
 import api from "./api";
 
-export const fetchDailyTrends = async (token: string): Promise<DailyTrendResponse> => {
+export const fetchDailyTrends = async (token: string, days: number): Promise<DailyTrendResponse> => {
   const res = await api.get(`/trend/daily`, {
-    headers: { Authorization: `Bearer ${token}` }
+    headers: { Authorization: `Bearer ${token}` },
+    params: { days },
   });
   return res.data;
 };
 
-export const fetchWeeklyTrends = async (token: string): Promise<WeeklyTrendResponse> => {
+export const fetchWeeklyTrends = async (token: string, weeks: number): Promise<WeeklyTrendResponse> => {
   const res = await api.get(`/trend/weekly`, {
-    headers: { Authorization: `Bearer ${token}` }
+    headers: { Authorization: `Bearer ${token}` },
+    params: { weeks },
   });
   return res.data;
 };
--- productivity-frontend/src/components/CarouselHeader.tsx
+++ productivity-frontend/src/components/CarouselHeader.tsx
@@ -7,30 +7,30 @@ const CarouselHeader = () => {
     const token = localStorage.getItem("token");
 
     const handleLogout = () => {
-        localStorage.removeItem("token");
-        navigate("/login")
-    };
+       localStorage.removeItem("token");
+       navigate("/login")
+   };
 
-    const isAuthPage = location.pathname === '/login' || location.pathname === '/signup';
+   const isAuthPage = location.pathname === '/login' || location.pathname === '/signup';
 
-    return (
-        <header className="w-full bg-primary text-text border-b border-border shadow-sm">
-            <div className="max-w-screen-xl mx-auto px-8 py-6 flex flex-col md:flex-row justify-between items-start md:items-center">
-                <div>
-                    <h1 className="text-4xl font-bold">Systemic</h1>
-                    <p className="text-accent text-sm italic mt-2 md:mt-0">your personal productivity tracker</p>
-                </div>
-                {!isAuthPage && token && (
-                    <button
-                    onClick={handleLogout}
-                    className="mt-4 md:mt-0 bg-accent text-white px-4 py-2 rounded hover:brightness-110 transition"
-                    >
-                        Logout
-                    </button>
-                )}
-            </div>
-        </header>
-    );
+   return (
+       <header className="w-full bg-primary text-text border-b border-border shadow-sm">
+           <div className="max-w-screen-xl mx-auto px-8 py-6 flex flex-col md:flex-row justify-between items-start md:items-center">
+               <div>
+                   <h1 className="text-4xl font-bold">Systemic</h1>
+                   <p className="text-accent text-sm italic mt-2 md:mt-0 text-right">Productivity Tracker with trend analysis</p>
+               </div>
+               {!isAuthPage && token && (
+                   <button
+                   onClick={handleLogout}
+                   className="mt-4 md:mt-0 bg-accent text-white px-4 py-2 rounded hover:brightness-110 transition"
+                   >
+                       Logout
+                   </button>
+               )}
+           </div>
+       </header>
+   );
 }
 
 export default CarouselHeader;
